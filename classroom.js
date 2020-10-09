ca6734508116f2ad16b9424306090989ad86add2
markjoseph <markjornales@gmail.com>
2020-10-09 18:17:17 +0800

    new slidebar

--- classroom.js
+++ classroom.js
@@ -2,12 +2,12 @@ const buttonToggled = document.querySelector('.button-nav');
 const buttonClass = document.querySelector('.addClass');
 const dropdown =  document.querySelector('.drop-downmenu');
 const targetClass = document.querySelector('.addClass');
-buttonToggled.addEventListener('click',()=>{
-    document.querySelector('.content-menu').classList.toggle('btnOpen');
+buttonToggled.addEventListener('click',()=>{ 
+    document.querySelector('.btnOpen').classList.toggle('active');
     document.querySelector('body').classList.toggle('removeScroll');
 });
 buttonClass.addEventListener('click',()=>{
-    document.querySelector('.drop-downmenu').classList.toggle('openmenu')
+    document.querySelector('.drop-downmenu').classList.toggle('openmenu');
 })
 
 window.addEventListener('click',(event)=>{
